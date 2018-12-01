6f24b88474a5f545a22332b8fce270b8a103984a
spring2maz <spring2maz@gmail.com>
2018-12-01 18:31:01 +0100

    Workaround fix for compile_erl function
    Prior to this commit, cuttent ebin is added for erlc to run
    with -pa. So the compiler can search current path when processing
    -include_lib.
    However, this makes erlang code server to cut off dir name
    after the first hyphen to get app name.
    e.g. if a erl app named foo_bar is in dir foo-bar, the app name
    would be taken as foo regardless of what is in the .app file.
    
    In this commit, path is added using -pz.
    This is ok because it only affects include_lib
    we should include local hrl files using -include
    instead of -include_lib

--- erlang.mk
+++ erlang.mk
@@ -1190,7 +1190,7 @@ ebin/:
 
 define compile_erl
 	$(erlc_verbose) erlc -v $(if $(IS_DEP),$(filter-out -Werror,$(ERLC_OPTS)),$(ERLC_OPTS)) -o ebin/ \
-		-pa ebin/ -I include/ $(filter-out $(ERLC_EXCLUDE_PATHS),$(COMPILE_FIRST_PATHS) $(1))
+		-pz ebin/ -I include/ $(filter-out $(ERLC_EXCLUDE_PATHS),$(COMPILE_FIRST_PATHS) $(1))
 endef
 
 ebin/$(PROJECT).app:: $(ERL_FILES) $(CORE_FILES) $(wildcard src/$(PROJECT).app.src)
